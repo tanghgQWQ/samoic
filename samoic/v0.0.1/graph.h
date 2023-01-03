de6ec75bb3051e5eb0fb0ab5bc89e4aa4898934e
tanghg <111404915+tanghgQWQ@users.noreply.github.com>
2023-01-03 08:43:23 +0800

    Update graph.h
    删除了原有的随机数部分，将它放入了rand.h中

--- samoic/v0.0.1/graph.h
+++ samoic/v0.0.1/graph.h
@@ -8,59 +8,9 @@
 #include <random>
 #include <chrono>
 #include <map>
+#include "rand.h"
 using namespace std;
 typedef long long ll;
-namespace Rand {
-    ll aaa;
-    double bbb;
-
-    ll rand_l(ll l, ll r, ll t = 1) {
-        if(l>r){swap(l,r);}
-        mt19937 R(chrono::system_clock::now().time_since_epoch().count());
-        uniform_int_distribution<ll> Rand(l, r);
-        aaa = Rand(R);
-        return aaa * t;
-    }
-
-    double rand_d(double l, double r) {
-        mt19937 R(chrono::system_clock::now().time_since_epoch().count());
-        uniform_real_distribution<double> Rand(l, r);
-        bbb = Rand(R);
-        return bbb;
-    }
-
-    string rand_c(ll len, char type) {
-        string s;
-        if (type == 'a') {
-            for (int i = 0; i < len; ++i) { s += char(rand_l('a', 'z')); }
-        } else if (type == 'A') {
-            for (int i = 0; i < len; ++i) { s += char(rand_l('A', 'Z')); }
-        } else if (type == '0') {
-            if (len == 1) {
-                s += char(rand_l('0', '9'));
-                return s;
-            }
-            s += char(rand_l('1', '9'));
-            for (int i = 1; i < len; ++i) { s += char(rand_l('0', '9')); }
-        } else if (type == 'w') {
-            s += char(rand_l('A', 'Z'));
-            for (int i = 1; i < len; ++i) { s += char(rand_l('a', 'z')); }
-        }
-        else if (type == 'l') {
-            for (int i = 0; i < len; ++i) { s += char(rand_l(33, 'z')); }
-        } else {
-            for (int i = 0; i < len; ++i) {
-                ll t = rand_l(1, 3);
-                if (t == 1) { s += char(rand_l('a', 'z')); }
-                else if (t == 2) {
-                    s += char(rand_l('A', 'Z'));
-                }
-                else { s += char(rand_l('0', '9')); }
-            }
-        }
-        return s;
-    }
-}using namespace Rand;
 class Graph{
 public:
     bool have_weight= false;//有权图或无权图
@@ -123,18 +73,6 @@ public:
                     adjacency_matrix[v][u]=w;
                 }else{
                     adjacency_matrix[v][u]=1;
-                }
-            }
-        }
-    }
-    void print(){
-        printf("%lld %lld\n",n,m);
-        for (int i = 0; i <g.size() ; ++i) {
-            for (auto v:g[i].path) {
-                if(!have_weight){
-                    printf("%lld %lld\n",g[i].u,v.v);
-                }else{
-                    printf("%lld %lld %lld\n",g[i].u,v.v,v.weight);
                 }
             }
         }
